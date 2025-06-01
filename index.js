18cd6581ffe724c33f2878f8fe505a87d1f315bb
omar-nuhman <159123441+omar-nuhman@users.noreply.github.com>
2025-06-01 19:37:21 +0530

    Fix date parameter handling in index.js

--- index.js
+++ index.js
@@ -1,54 +1,44 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
 app.get("/api/:date?", function (req, res) {
-  let dateParam = req.params.date;
+  const dateParam = req.params.date;
 
-  let dateObj;
+  let date;
 
-    if (!dateParam) {
-    dateObj = new Date();
-  }  else if (/^\d+$/.test(dateParam)) {
-    dateObj = new Date(parseInt(dateParam)); // Convert string to number
-  }  else {
-    // dateParam is a string date -> parse as string
-    dateObj = new Date(dateParam);
+  // Case 1: If no date is provided, return current date
+  if (!dateParam) {
+    date = new Date();
+  } 
+  // Case 2: If input is a number (milliseconds), parse it as number
+  else if (!isNaN(dateParam) && /^\d+$/.test(dateParam)) {
+    date = new Date(parseInt(dateParam));
+  } 
+  // Case 3: Input is string like "2015-12-25"
+  else {
+    date = new Date(dateParam);
   }
 
-  if(dateObj.toString()==="Invalid Date"){
-
+  // If invalid, return error
+  if (date.toString() === "Invalid Date") {
     return res.json({ error: "Invalid Date" });
   }
 
   res.json({
-    unix: dateObj.getTime(),
-    utc: dateObj.toUTCString()
-  })
-
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
-
-
-// Listen on port set in environment variable or default to 3000
+// Listener
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
